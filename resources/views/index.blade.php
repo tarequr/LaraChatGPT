c1bb8908e0c386e14347d3f2cd68797605da5f1e
tarequr <tarequrr8@gmail.com>
2023-03-11 23:53:57 +0600

    design for submit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <div class="p-3">
+            <div class="p-3 rounded text-light" style="background: blueviolet">
+                <h4>Laravel ChatGpt</h4>
+                <p>Message</p>
+            </div>
+        </div>
+
+        <div class="row">
+
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
